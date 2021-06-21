ca773d470906d7cac8834fa1ac6afc408628741c
Galiwango Ananiya <galiwango101@gmail.com>
2021-06-22 02:57:37 +0300

    Add a method to build the bunnies for the game.

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -17,6 +17,21 @@ export default class GameScene extends Phaser.Scene {
     this.buildBunnies();
   }
 
+  buildBunnies() {
+    this.bunnyGroup = this.add.group();
+    this.bunnyGroup.enableBody = true;
+    let atlasTexture = this.textures.get('bunnyAtlas');
+    let frames = atlasTexture.getFrameNames();
+    for(let i=0; i<this.totalBunnies; i++) {
+      let x = Phaser.Math.Between(-10, this.game.renderer.width-50);
+      let y = Phaser.Math.Between(this.game.renderer.height-180, this.game.renderer.height-60);
+      let b = this.bunnyGroup.create(x, y, 'bunnyAtlas',frames[i]);
+      this.assignBunnyMovement(b);
+    }
+  }
+
+
+
   update() {}
 
 }

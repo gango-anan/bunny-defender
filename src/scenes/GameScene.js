553cfaa5d76f94eeb539b79200f554773e8720f3
Galiwango Ananiya <galiwango101@gmail.com>
2021-06-22 02:55:52 +0300

    Add buildWorld method that constructs the Game world.

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -10,5 +10,13 @@ export default class GameScene extends Phaser.Scene {
     this.buildWorld();
   }
 
+  buildWorld = ()=> {
+    this.add.image(0, 0, 'sky').setOrigin(0, 0);
+    this.add.image(270, 780, 'hill');
+    this.add.image(270, 880, 'hill');
+    this.buildBunnies();
+  }
+
+  update() {}
 
 }

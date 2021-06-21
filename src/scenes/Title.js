4baeef599deead2b2b2b6bd358d96f7cf57d999b
Galiwango Ananiya <galiwango101@gmail.com>
2021-06-21 17:23:28 +0300

    Add interactivity capability to the play button.

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -7,6 +7,23 @@ export default class Title extends Phaser.Scene {
     this.add.image(0, 0, 'titleBg').setOrigin(0,0);
     let playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2 + 150, 'playBtn');
     let optionsButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2 + 225, 'optionsBtn');
+ 
 
+    this.sound.play('bgAudio', {
+      loop: true
+    })
+
+    playButton.setInteractive({useHandCursor: true});
+    playButton.on('pointerover', () => {
+
+    })
+
+    playButton.on('pointerout', () => {
+
+    })
+
+    playButton.on('pointerup', () => {
+
+    })
   }
 } 

7dcda5a33ed5c2e0034b8cebe363979d671252ab
Galiwango Ananiya <galiwango101@gmail.com>
2021-06-24 17:43:47 +0300

    Add class to handle the functionality of create and firing a bullet.

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,3 +1,16 @@
+class Laser extends Phaser.Physics.Arcade.Sprite {
+	constructor(scene, x, y) {
+		super(scene, x, y, 'laser');
+	}
+
+	fire(x, y) {
+		this.body.reset(x, y);
+		this.setActive(true);
+		this.setVisible(true);
+		this.setVelocityY(-900);
+	}
+}
+
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super('GameScene');

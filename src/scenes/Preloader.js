a7cd7c52d98171f6e1eca086bfe97afba123cf61
Galiwango Ananiya <galiwango101@gmail.com>
2021-06-25 21:36:11 +0300

    Add game menu and refactor code base to organize the code.

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -18,9 +18,6 @@ export default class Preloader extends Phaser.Scene {
       progressBar.fillStyle(0xff9200, 1);
       progressBar.fillRect(90, 612, 360 * value, 25);
     });
-
-    this.load.on('fileprogress', (file) => {
-    });
     
     this.load.on('complete', () => {
         progressBar.destroy();
@@ -32,26 +29,24 @@ export default class Preloader extends Phaser.Scene {
     this.load.image('explosion', 'assets/images/explosion.png');
     this.load.image('ghost', 'assets/images/ghost.png');
     this.load.image('hill', 'assets/images/hill.png');
-    this.load.image('loaderBar', 'assets/images/loader_bar.png');
     this.load.image('sky', 'assets/images/sky.png');
-    this.load.image('optionsBtn', 'assets/images/options_button.png');
-    this.load.image('playBtn', 'assets/images/play_button.png');
-    this.load.spritesheet('bunny', "assets/images/spritesheets/bunny.png", {
-      frameHeight: 70,
-      frameWidth: 64
-    });
+    this.load.spritesheet('bunny', "assets/images/spritesheets/bunny.png", { frameHeight: 70, frameWidth: 64 });
     this.load.atlasXML('bunnyAtlas', 'assets/images/spritesheets/bunny.png', 'assets/images/spritesheets/bunny.xml')
     this.load.atlasXML('spaceRock', 'assets/images/spritesheets/SpaceRock.png', 'assets/images/spritesheets/SpaceRock.xml');
     this.load.image('explosion', 'assets/images/explosion.png');
     this.load.image('ghost', 'assets/images/ghost.png');
     this.load.image('laser', 'assets/images/bullet.png');
     this.load.image('player', 'assets/images/player.png');
+    this.load.image('buttons', 'assets/images/buttons.png');
     this.load.audio('bgAudio', 'assets/audio/bgm.mp3');
     this.load.audio('explosion', 'assets/audio/explosion.mp3');
     this.load.audio('select', 'assets/audio/select.mp3');
     this.load.audio('hurt', 'assets/audio/hurt.mp3');
-    
-    for(let i = 0; i<1000; i++) {
+    this.monitorProgressBar();
+  }
+
+  monitorProgressBar() {
+    for(let i = 0; i<500; i++) {
       this.load.image('logo'+i, 'assets/images/TitleImage.png');
     }
   }
--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -3,42 +3,64 @@ import GameScene from "./GameScene";
 export default class Title extends Phaser.Scene {
   constructor() {
     super('Title');
+    this.dimensions;
+    this.mainAudio;
   }
 
   create() {
+    this.dimensions = [this.game.renderer.width*0.5, this.game.renderer.height*0.5-100];
+    this.playSceneMusic();
     this.add.image(0, 0, 'sky').setOrigin(0,0);
     this.add.image(270, 200, 'title');
-    let playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2 + 150, 'playBtn');
-    let optionsButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2 + 225, 'optionsBtn');
-    let hoverSprite = this.add.sprite(100, 100, 'bunny');
-    hoverSprite.setScale(0.5);
-    hoverSprite.setVisible(false);
-    this.anims.create({
-      key: 'walk',
-      frameRate: 8,
-      repeat: -1,
-      frames: this.anims.generateFrameNumbers('bunny', {frames: [0,1,2,3,4,5,6,7]})
-    })
+    this.buildMenu(this.dimensions);
+    this.addRunningBunny();
+  }
 
-    // this.sound.play('bgAudio', {
-    //   loop: true
-    // })
+  playSceneMusic() {
+    this.mainAudio = this.sound.add('bgAudio', { volume: 0.1, loop: true });
+    this.mainAudio.play();
+  }
 
+  buildMenu(dimensions) {
+    const [ xCord, yCord ] = dimensions;
+    const playButton = this.add.image(xCord, yCord, 'buttons').setDisplaySize(200, 50);
+    this.add.text(playButton.x, playButton.y, 'Play', { fontSize: '24px', fill: '#fff' }).setOrigin(0.5);
+    const optionsButton = this.add.image(xCord, yCord+80, 'buttons').setDisplaySize(200, 50);
+    this.add.text(optionsButton.x, optionsButton.y, 'Options', { fontSize: '24px', fill: '#fff' }).setOrigin(0.5);
+    const creditsButton = this.add.image(xCord, yCord+160, 'buttons').setDisplaySize(200, 50);
+    this.add.text(creditsButton.x, creditsButton.y, 'Credits', { fontSize: '24px', fill: '#fff' }).setOrigin(0.5);
+    const leaderBoardButton = this.add.image(xCord, yCord+240, 'buttons').setDisplaySize(200, 50);
+    this.add.text(leaderBoardButton.x, leaderBoardButton.y, 'Leader Board', { fontSize: '24px', fill: '#fff' }).setOrigin(0.5);
+    const menuButtons = [playButton, optionsButton, creditsButton, leaderBoardButton];
+    this.addButtonInteractions(menuButtons);
+    this.addButtonEvents(menuButtons);
+  }
+
+  addButtonInteractions(menuButtons) {
+    const[playButton, optionsButton, creditsButton, leaderBoardButton] = menuButtons;
     playButton.setInteractive({useHandCursor: true});
-    playButton.on('pointerover', () => {
-      hoverSprite.setVisible(true);
-      hoverSprite.play('walk');
-      hoverSprite.x = playButton.x - playButton.width;
-      hoverSprite.y = playButton.y;
-    })
+    optionsButton.setInteractive({useHandCursor: true});
+    creditsButton.setInteractive({useHandCursor: true});
+    leaderBoardButton.setInteractive({useHandCursor: true});
+  }
 
-    playButton.on('pointerout', () => {
-      hoverSprite.setVisible(false);
-    })
-
+  addButtonEvents(menuButtons) {
+    const[playButton, optionsButton, creditsButton, leaderBoardButton] = menuButtons;
     playButton.on('pointerup', () => {
       this.scene.add('GameScene', GameScene, true);
       this.scene.remove('Title');
     })
+  }
+
+  addRunningBunny() {
+    const hoverSprite = this.add.sprite(270, 200, 'bunny');
+    hoverSprite.setScale(0.8);
+    this.anims.create({
+      key: 'walk',
+      frameRate: 8,
+      repeat: -1,
+      frames: this.anims.generateFrameNumbers('bunny', {frames: [0 , 1, 2, 3, 4, 5, 6, 7]})
+    })
+    hoverSprite.play('walk');
   }
 } 

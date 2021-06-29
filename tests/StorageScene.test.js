39c0e3ff21b9918e6fdbf8070a1b580e1bbea4a1
Galiwango Ananiya <galiwango101@gmail.com>
2021-06-29 22:26:30 +0300

    Add tests for Storage Scene module.

---
+++ tests/StorageScene.test.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+import StorageScene from '../src/scenes/StorageScene'
+
+const storageScene = new StorageScene();
+
+test('StorageScene correctly initializes a StorageScene object' , () => {
+  expect(typeof storageScene).toBe('object');
+});
+
+test('StorageScene correctly initializes a StorageScene object' , () => {
+  expect(typeof StorageScene).not.toBe('undefined');
+});
+
+test('StorageScene is a function' , () => {
+  expect(typeof StorageScene).toBe('function');
+});
+
+test('StorageScene is a subclass of Phaser.Scene', () => {
+  expect(StorageScene).toBeSubclassOf(Phaser.Scene);
+});
+
+test('StorageScene is not a subclass of Phaser.Physics.Arcade', () => {
+  expect(StorageScene).not.toBeSubclassOf(Phaser.Physics.Arcade.Body);
+});

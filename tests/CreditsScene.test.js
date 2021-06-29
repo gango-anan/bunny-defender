cc29072d9251d7097290f0a7386772bd4a0b6862
Galiwango Ananiya <galiwango101@gmail.com>
2021-06-29 22:15:57 +0300

    Add tests for GameScene module.

---
+++ tests/CreditsScene.test.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+import CreditsScene from '../src/scenes/CreditsScene'
+
+const creditsScene = new CreditsScene();
+
+test('CreditsScene correctly initializes a Credits Scene object' , () => {
+  expect(typeof creditsScene).toBe('object');
+});
+
+test('CreditsScene correctly initializes a Credits Scene object' , () => {
+  expect(typeof CreditsScene).not.toBe('undefined');
+});
+
+test('CreditsScene is a function' , () => {
+  expect(typeof CreditsScene).toBe('function');
+});
+
+test('CreditsScene is a subclass of Phaser.Scene', () => {
+  expect(CreditsScene).toBeSubclassOf(Phaser.Scene);
+});
+
+test('CreditsScene is not a subclass of Phaser.Physics.Arcade', () => {
+  expect(CreditsScene).not.toBeSubclassOf(Phaser.Physics.Arcade.Body);
+});
---
+++ tests/GameScene.test.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+import GameScene from '../src/scenes/GameScene'
+
+const gameScene = new GameScene();
+
+test('GameScene correctly initializes a Credits Scene object' , () => {
+  expect(typeof gameScene).toBe('object');
+});
+
+test('GameScene correctly initializes a Credits Scene object' , () => {
+  expect(typeof GameScene).not.toBe('undefined');
+});
+
+test('GameScene is a function' , () => {
+  expect(typeof GameScene).toBe('function');
+});
+
+test('GameScene is a subclass of Phaser.Scene', () => {
+  expect(GameScene).toBeSubclassOf(Phaser.Scene);
+});
+
+test('GameScene is not a subclass of Phaser.Physics.Arcade', () => {
+  expect(GameScene).not.toBeSubclassOf(Phaser.Physics.Arcade.Body);
+});
